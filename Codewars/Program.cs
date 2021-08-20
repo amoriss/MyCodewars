31cc1d018ca4940d8b9c029c731cd7bd62ef826b
Amoriss <amoriss@protonmail.com>
2021-08-19 21:32:08 -0500

    first code kata

---
+++ Codewars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Codewars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //public static bool IsLockNessMonster(string sentence)
+        //{
+        //    // FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+        //    if (sentence.ToLower().Contains("tree fiddy") || sentence.Contains("3.50"))
+        //    {
+        //        return true;
+        //    }
+
+        //    else
+        //    {
+        //        return false;
+        //    }
+
+
+        //}
+
+    }
+}
